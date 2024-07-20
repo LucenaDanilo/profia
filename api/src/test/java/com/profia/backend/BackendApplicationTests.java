14948299509a07d2914db1a2a78ca9b26f2e7564
Profia <profia@Profias-Mac-mini.local>
2024-07-20 17:29:31 -0300

    subindo back

---
+++ api/src/test/java/com/profia/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
