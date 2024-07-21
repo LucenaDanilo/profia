312753392f2728fa906c201a0a9915606b2ff84c
gustavo-ataide <gugaben903@gmail.com>
2024-07-21 02:41:26 -0300

    Products entity

---
+++ api/src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,63 @@
+package com.example.springboot.models;
+
+
+import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUTOS")
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+    private String description;
+    private String image;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
