3b749e75109a40117df84d30fab0901ebbb1cfa2
SolracSiul <crluis521@gmail.com>
2024-08-07 20:15:19 -0300

    change nextconfig and add session

--- public/my-app/next.config.mjs
+++ public/my-app/next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
+  env:{
+    NEXTAUTH_SECRET : process.env.NEXTAUTH_SECRET ?? ''
+  },
+  images: {
+      remotePatterns: [
           {
-            protocol: 'https',
-            hostname: 'assets.example.com',
-            port: '',
-            pathname: '/account123/**',
+              protocol: 'https',
+              hostname: 'assets.example.com',
+              port: '',
+              pathname: '/account123/**',
           },
-        ],
-      },
+      ],
+  },
 };
 
 export default nextConfig;
--- public/my-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Site da profia lindo maravilhoso</h1>
-    </div>
-  );
-}
