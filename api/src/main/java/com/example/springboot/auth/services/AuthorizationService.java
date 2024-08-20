dd7488f88a07e6830ffb07d9c0702aeee6794fef
gustavo-ataide <gugaben903@gmail.com>
2024-08-20 14:24:50 -0300

    fix: error message when email/password is incorrect.

--- api/src/main/java/com/example/springboot/auth/services/AuthorizationService.java
+++ api/src/main/java/com/example/springboot/auth/services/AuthorizationService.java
@@ -13,9 +13,12 @@ import com.example.springboot.security.TokenService;
 import com.example.springboot.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -66,65 +69,72 @@ public class AuthorizationService implements UserDetailsService{
     }
 
     public ResponseEntity<Object> login(@RequestBody @Valid AuthetinticationDto data) {
-        authenticationManager = context.getBean(AuthenticationManager.class);
+        try {
+            authenticationManager = context.getBean(AuthenticationManager.class);
 
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
+            var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+            var auth = this.authenticationManager.authenticate(usernamePassword);
 
-        // Persistir a autenticação no SecurityContext
-        SecurityContextHolder.getContext().setAuthentication(auth);
+            // Persistir a autenticação no SecurityContext
+            SecurityContextHolder.getContext().setAuthentication(auth);
 
+            var token = tokenService.generateToken((UserModel) auth.getPrincipal());
 
-        var token = tokenService.generateToken((UserModel) auth.getPrincipal());
+            // Obter detalhes do usuário autenticado
+            UserModel user = (UserModel) auth.getPrincipal();
 
-        // Obter detalhes do usuário autenticado
-        UserModel user = (UserModel) auth.getPrincipal();
-        System.out.println(data.password());
+            // Criar o DTO apropriado com base no tipo de usuário
+            Object userDto;
+            if (user instanceof Teacher) {
+                Teacher teacher = (Teacher) user;
+                TeacherDto teacherDto = new TeacherDto();
+                teacherDto.setId(teacher.getId());
+                teacherDto.setName(teacher.getName());
+                teacherDto.setEmail(teacher.getEmail());
+                teacherDto.setUserRole(teacher.getUserRole());
+                teacherDto.setCnpj(teacher.getCnpj());
+                teacherDto.setHrAula(teacher.getHrAula());
+                teacherDto.setEspecialidade(teacher.getEspecialidade());
+                userDto = teacherDto;
+            } else if (user instanceof Student) {
+                Student student = (Student) user;
+                StudentDto studentDto = new StudentDto();
+                studentDto.setId(student.getId());
+                studentDto.setName(student.getName());
+                studentDto.setEmail(student.getEmail());
+                studentDto.setUserRole(student.getUserRole());
+                studentDto.setResponsibleCPF(student.getResponsibleCPF());
+                studentDto.setRegistration(student.getRegistration());
+                studentDto.setBirthday(student.getBirthday());
+                studentDto.setPoints(student.getPoints());
+                userDto = studentDto;
+            } else if (user instanceof Manager) {
+                Manager manager = (Manager) user;
+                ManagerDto managerDto = new ManagerDto();
+                managerDto.setId(manager.getId());
+                managerDto.setName(manager.getName());
+                managerDto.setEmail(manager.getEmail());
+                managerDto.setUserRole(manager.getUserRole());
+                managerDto.setSuperuser(manager.isSuperuser());
+                userDto = managerDto;
+            } else {
+                userDto = new UserDto();
+                ((UserDto) userDto).setId(user.getId());
+                ((UserDto) userDto).setName(user.getName());
+                ((UserDto) userDto).setEmail(user.getEmail());
+                ((UserDto) userDto).setUserRole(user.getUserRole());
+            }
 
-        // Criar o DTO apropriado com base no tipo de usuário
-        Object userDto;
-        if (user instanceof Teacher) {
-            Teacher teacher = (Teacher) user;
-            TeacherDto teacherDto = new TeacherDto();
-            teacherDto.setId(teacher.getId());
-            teacherDto.setName(teacher.getName());
-            teacherDto.setEmail(teacher.getEmail());
-            teacherDto.setUserRole(teacher.getUserRole());
-            teacherDto.setCnpj(teacher.getCnpj());
-            teacherDto.setHrAula(teacher.getHrAula());
-            teacherDto.setEspecialidade(teacher.getEspecialidade());
-            userDto = teacherDto;
-        } else if (user instanceof Student) {
-            Student student = (Student) user;
-            StudentDto studentDto = new StudentDto();
-            studentDto.setId(student.getId());
-            studentDto.setName(student.getName());
-            studentDto.setEmail(student.getEmail());
-            studentDto.setUserRole(student.getUserRole());
-            studentDto.setResponsibleCPF(student.getResponsibleCPF());
-            studentDto.setRegistration(student.getRegistration());
-            studentDto.setBirthday(student.getBirthday());
-            studentDto.setPoints(student.getPoints());
-            userDto = studentDto;
-        } else if (user instanceof Manager) {
-            Manager manager = (Manager) user;
-            ManagerDto managerDto = new ManagerDto();
-            managerDto.setId(manager.getId());
-            managerDto.setName(manager.getName());
-            managerDto.setEmail(manager.getEmail());
-            managerDto.setUserRole(manager.getUserRole());
-            managerDto.setSuperuser(manager.isSuperuser());
-            userDto = managerDto;
-        } else {
-            userDto = new UserDto();
-            ((UserDto) userDto).setId(user.getId());
-            ((UserDto) userDto).setName(user.getName());
-            ((UserDto) userDto).setEmail(user.getEmail());
-            ((UserDto) userDto).setUserRole(user.getUserRole());
+            // Retornar o token e os dados do usuário
+            return ResponseEntity.ok(new LoginResponseDto(token, userDto));
+
+        } catch (BadCredentialsException e) {
+            // Retornar uma resposta com status 401 e mensagem de erro
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou senha incorretos.");
+        } catch (AuthenticationException e) {
+            // Tratar outras exceções de autenticação se necessário
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Erro ao tentar autenticar.");
         }
-
-        // Retornar o token e os dados do usuário
-        return ResponseEntity.ok(new LoginResponseDto(token, userDto));
     }
 
 
